f171a8d1d846356504774a12fa235561dec9510a
ajfisher <ajfisher.td@gmail.com>
2017-06-15 21:23:08 +1000

    Add dev server to provide static assets

---
+++ server/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const http = require('http');
+const express = require('express');
+
+const port = 3002;
+const host = 'localhost';
+
+var app = express();
+
+
+var server = http.createServer(app);
+
+app.use('/media', express.static('media'))
+
+server.listen(port, host, () => {
+    console.log(`Static Server now listening on ${host}:${port}`);
+});
+
